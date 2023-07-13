250efd69afe52ff980f2a797abda7252a2817adc
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-07-13 17:23:15 +0900

    added todo items into borad

---
+++ trello/src/theme.ts
@@ -0,0 +1,15 @@
+import "styled-components";
+
+interface DefaultTheme {
+    backgroundColor?: string;
+    boardColor?: string;
+    cardColor?: string;
+    fontColor?: string;
+}
+
+export const defaultTheme: DefaultTheme = {
+    backgroundColor: "#54a0ff",
+    boardColor: "#c8d6e5",
+    cardColor: "white",
+    fontColor: "#2d3436",
+};
