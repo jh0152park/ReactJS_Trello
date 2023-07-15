62faece5fc5956bf99ab1513811e692d112b4d33
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-07-15 22:20:44 +0900

    fixed create board name input

--- trello/src/components/AddBoard.tsx
+++ trello/src/components/AddBoard.tsx
@@ -1,5 +1,43 @@
+import { styled } from "styled-components";
+
+const AddItem = styled.form`
+    input {
+        margin: 10px 10px;
+        width: 400px;
+        height: 20px;
+        border-radius: 10px;
+        padding-left: 10px;
+        border: none;
+
+        position: absolute;
+        left: 100px;
+        bottom: 15px;
+
+        background-color: ${(props) => props.theme.cardColor};
+        &::placeholder {
+            color: ${(props) => props.theme.fontColor};
+            font-size: 15px;
+        }
+    }
+`;
+
 function AddBoard() {
-    return null;
+    return (
+        // <AddItem onSubmit={handleSubmit(onSubmit)}>
+        <AddItem>
+            <input
+                // style={{
+                //     opacity: addButton ? 1 : 0,
+                //     transition: "opacity 0.2s ease-in-out",
+                // }}
+                // {...register("item", {
+                //     required: true,
+                // })}
+                type="text"
+                placeholder="please let me know the name of new action item board."
+            ></input>
+        </AddItem>
+    );
 }
 
 export default AddBoard;
